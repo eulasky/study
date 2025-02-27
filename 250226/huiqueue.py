e13d1a81502ed9a103070c47adbc9fdd54e8ae0a
kkkmmh <kmh981102@gmail.com>
2025-02-27 17:59:04 +0900

    250227

--- 250226/huiqueue.py
+++ 250226/huiqueue.py
@@ -5,20 +5,21 @@ numbers = [i for i in range(1, N + 1)]
 q = []
 
 cnt = 0
-for i in numbers:
+for i in select:
     j = 0
-    while numbers[j] != i:
+    while j < len(numbers) and numbers[j] != i:
         q.append(numbers.pop(0))
-        j += 1
 
     numbers.pop(0)
 
-    if len(numbers) > len(q):
-        cnt += len(q) + 1
+    if len(numbers) >= len(q):
+        cnt += len(q)
         numbers += q
 
     else:
         cnt += len(numbers) + 1
         numbers += q
+
+    q = []
 
 print(cnt)
