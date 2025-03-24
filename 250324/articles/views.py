1511320a5f514a6b1b32357619a520078037c0c3
eulasky <yuurisa785@gmail.com>
2025-03-24 13:30:39 +0900

    250324

---
+++ 250324/articles/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    # 응답 객체를 반환
+    # 메인 페이지가 담겨있는 응답 객체를 반환
+    return render(request, 'articles/index.html')
+    
