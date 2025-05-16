d275efd5f06d52897e41d49c426318be7ecebdcd
eula <anisia1231@naver.com>
2025-05-16 17:36:23 +0900

    thread-2

---
+++ 0. 관통/250516/vue-project/src/views/GetMyLocationView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="loadLocation">내 위치 가져오기</button>
+    <!-- 왜 v-show가 아니라 v-if를 썼을까 -->
+    <p v-if="lat && lng">Lat: {{ lat }}, Lng: {{ lng }}</p>
+    <p v-if="error">{{ error }}</p>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const lat = ref(null)
+const lng = ref(null)
+const error = ref('')
+
+const loadLocation = () => {
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      lat.value = pos.coords.latitude
+      lng.value = pos.coords.longitude
+    },
+    (err) => { 
+      error.value = `위치 조회 실패: ${err.message}` 
+    },
+  )
+}
+
+</script>
+
